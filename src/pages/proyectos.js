98f55936f04602e3d044a4ef87dca472a1e778f8
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-04-25 21:38:55 -0500

    Update

--- src/pages/proyectos.js
+++ src/pages/proyectos.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Layout from "../layout";
 import { proyectos } from "../hooks/countProyects";
 
